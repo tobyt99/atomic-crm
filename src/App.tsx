807e893692bae3a3e7fbbdc7428e114861172f3b
digitalt <toby.treacher@gmail.com>
2025-08-01 11:35:40 +0100

    add Vester branding: custom theme colors, logo, favicon, and Gabarito font

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { CRM } from './root/CRM';
 import { radiantLightTheme, radiantDarkTheme } from 'react-admin';
+import { deepmerge } from '@mui/utils';
 
 /**
  * Application entry point
@@ -20,22 +21,40 @@ import { radiantLightTheme, radiantDarkTheme } from 'react-admin';
  *
  * @example
  *
-  const App = () => (
-     <CRM
-        logo="./img/logo.png"
-        title="Elira"
-        lightTheme={radiantLightTheme}
-        darkTheme={radiantDarkTheme}
-     />
-  );
+ * const App = () => (
+ *   <CRM
+ *       logo="./img/logo.png"
+ *       title="Elira"
+ *       lightTheme={radiantLightTheme}
+ *       darkTheme={radiantDarkTheme}
+ *    />
+ * );
  */
 
+// Custom theme with modified primary color
+const customLightTheme = deepmerge(radiantLightTheme, {
+  palette: {
+    primary: {
+      main: '#2B2D42', // Vester dark blue
+    },
+  },
+});
+
+const customDarkTheme = deepmerge(radiantDarkTheme, {
+  palette: {
+    primary: {
+      main: '#98DFEA', // Vester light blue
+    },
+  },
+});
+
 const App = () => (
-    <CRM
-        title="Elira"
-        lightTheme={radiantLightTheme}
-        darkTheme={radiantDarkTheme}
-    />
+  <CRM
+    logo="./img/vester_logo_white.png"
+    title="Elira"
+    lightTheme={customLightTheme}
+    darkTheme={customDarkTheme}
+  />
 );
 
 export default App;
